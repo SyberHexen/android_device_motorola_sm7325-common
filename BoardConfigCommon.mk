af27a4c0362b284a27d28b95233cb0b6934142de
Andrew Hexen <SyberHexen@gmail.com>
2022-01-05 15:42:39 -0800

    sm7250-common: Lineage -> PE
    Change-Id: Ifaa698f44065afeba0a2a13b6d07ebb159f9d068

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,7 +123,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
