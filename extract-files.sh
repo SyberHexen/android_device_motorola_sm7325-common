2d290b154e6aa21c8445cc7cbe60591033ab30b4
Alexander Winkowski <dereference23@outlook.com>
2022-09-04 06:02:52 +0000

    Revert "sm7325-common: Control wfdservice64 with vendor.wfdservice prop"
    This reverts commit 35e348314f1f2807bb93227a2beb5c7a307d8881.

--- extract-files.sh
+++ extract-files.sh
@@ -63,9 +63,6 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             ${PATCHELF} --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
-        system_ext/etc/init/wfdservice.rc)
-            sed -i "/^service/! s/wfdservice$/wfdservice64/g" "${2}"
-            ;;
         system_ext/etc/permissions/moto-telephony.xml)
             sed -i "s#/system/#/system_ext/#" "${2}"
             ;;
